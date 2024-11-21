ad6e481f76fd00f0bd195be8e963b92dcb8248d8
Tejas-Santosh-Nalawade <tejassantoshnalawade@gmail.com>
2024-11-21 13:13:33 +0530

    function basics

---
+++ Functions/function_basic.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int printHello()
+{
+    cout<<"Hello World"<<"\n";
+    return 3;
+
+}
+
+int main()
+{
+    // int val=printHello();
+    cout<<printHello()<<"\n";
+
+    return 0;
+}
